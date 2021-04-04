5400ef071d0179ca40b42786b0fdf6e4969c9b2f
Ahmed Embaby <ahmed00798@eng.sohag.edu.eg>
2021-04-04 13:42:14 +0200

    add dd

---
+++ PSC Cost Control/Repositories/BOQItemsRegisterationRepositories/BOQitemRegisterationRepo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSC_Cost_Control.Repositories.BOQItemsRegisterationRepositories
+{
+    class BOQitemRegisterationRepo
+    {
+    }
+}
