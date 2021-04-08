7aefabc95c99c679e569853eec9c1beecd5fc6a1
Ahmed Embaby <engamaeae@gmail.com>
2021-04-08 16:39:54 +0200

    Solving bugs in SP for project Codes and unified codes

--- PSC Cost Control/Models/Entities.Designer.cs
+++ PSC Cost Control/Models/Entities.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Mr\Desktop\PSC3\PSC-Cost-Control\PSC Cost Control\Models\Entities.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\pc\Desktop\psc3\PSC-Cost-Control\PSC Cost Control\Models\Entities.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
---
+++ PSC Cost Control/Services/ServicesBuilders/ServiceBuilder.cs
@@ -0,0 +1,9 @@
+﻿namespace PSC_Cost_Control.Services.ServicesBuilders
+{
+    public static class ServiceBuilder
+    {
+        /**public static T Build<T>() { 
+
+        }*//
+    }
+}
